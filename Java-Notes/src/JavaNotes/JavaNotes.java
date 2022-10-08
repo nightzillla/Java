6946a26fc033a08bbedd8ccb73cc5dda693181b4
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-08 16:03:42 -0500

    learning java data types

--- Java-Notes/src/JavaNotes/JavaNotes.java
+++ Java-Notes/src/JavaNotes/JavaNotes.java
@@ -3,7 +3,9 @@ package JavaNotes;
 // meaning everything inside of something called classes, and
 // everything is known as objects
 
-// int means integer
+// int means integer any # that is not a decimal
+// double means floating decimal point
+// boolean true or false
 // _ represent a space
 // variables holds value
 public class JavaNotes {
@@ -11,7 +13,10 @@ public class JavaNotes {
     public static void main(String[] args) {      //  This is called a method
         // TOD Auto-generated method stub
         System.out.println("Hello World!");       // println means print line
+        // DATA TYPE
         int hello_world = 5;                      // This is a variable
+        double num2 = 5.0;
+        boolean b = true;
         System.out.println(hello_world);          // this prints out 5
     }
 }
