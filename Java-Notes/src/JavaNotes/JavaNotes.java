605ad2fab876819bfc87e0f29960bf4d292dff1b
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-08 11:47:13 -0500

    learning println

---
+++ Java-Notes/src/JavaNotes/JavaNotes.java
@@ -0,0 +1,14 @@
+package JavaNotes;
+// Java is an Object oriented programming language
+// meaning everything inside of something called classes, and
+// everything is known as objects
+
+
+public class JavaNotes {
+
+    public static void main(String[] args) {      //  This is called a method
+        // TOD Auto-generated method stub
+        System.out.println("Hello World!");       // println means print line
+        System.out.println("Hello World2!")
+    }
+}
