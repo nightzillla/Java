d4f7ee0911770459eda62c8a5d4fc486f6c7cc28
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-08 16:20:36 -0500

    order of operations

--- Java-Notes/src/JavaNotes/JavaNotes.java
+++ Java-Notes/src/JavaNotes/JavaNotes.java
@@ -6,6 +6,8 @@ package JavaNotes;
 // int means integer any # that is not a decimal
 // double means floating decimal point
 // boolean true or false
+// char represent any character or letter that's in = '' and CAN BE ONLY 1 letter
+// String has to be in double quotation marks = " "
 // _ represent a space
 // variables holds value
 public class JavaNotes {
@@ -13,10 +15,28 @@ public class JavaNotes {
     public static void main(String[] args) {      //  This is called a method
         // TOD Auto-generated method stub
         System.out.println("Hello World!");       // println means print line
-        // DATA TYPE
-        int hello_world = 5;                      // This is a variable
+        // Primitive DATA TYPE // not changeable
+        int hello_world = 9;                      // This is a variable
         double num2 = 5.0;
         boolean b = true;
-        System.out.println(hello_world);          // this prints out 5
+        char C = 'h';
+        // NOT primitive
+        String str = "tim";
+
+        int tim = hello_world;
+
+        System.out.println(tim);          // this prints out 5
+
+        int a;
+        a = 6; // have to initialized it first
+        // PEMDAS: Parentheses, Exponents, Multiplication, and Division (from left to right),
+        // Addition and Subtraction (from left to right).
+        int x = 5;
+        int y = 7;
+        int z = 56;
+        int sum = x + y + z;
+
+        System.out.println(sum);
+
     }
 }
