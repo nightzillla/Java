aa97c36787de51c49977b93ac806b89d5d62b478
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-08 11:53:41 -0500

    learning variables

--- Java-Notes/src/JavaNotes/JavaNotes.java
+++ Java-Notes/src/JavaNotes/JavaNotes.java
@@ -3,12 +3,15 @@ package JavaNotes;
 // meaning everything inside of something called classes, and
 // everything is known as objects
 
-
+// int means integer
+// _ represent a space
+// variables holds value
 public class JavaNotes {
 
     public static void main(String[] args) {      //  This is called a method
         // TOD Auto-generated method stub
         System.out.println("Hello World!");       // println means print line
-        System.out.println("Hello World2!")
+        int hello_world = 5;                      // This is a variable
+        System.out.println(hello_world);          // this prints out 5
     }
 }
