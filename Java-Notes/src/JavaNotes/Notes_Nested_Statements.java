54db66fec1eaa1d309eb97567c4faca18a18e7f6
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-09 12:48:37 -0500

    finshed nested statements

--- Java-Notes/src/JavaNotes/Notes_Nested_Statements.java
+++ Java-Notes/src/JavaNotes/Notes_Nested_Statements.java
@@ -1,6 +1,6 @@
 package JavaNotes;
 import java.util.Scanner;
-
+// Nesting is putting another if statement inside another if statement
 public class Notes_Nested_Statements {
     public static void main(String[] args) {
         System.out.print("Input your age: ");
@@ -8,10 +8,20 @@ public class Notes_Nested_Statements {
         String s = sc.nextLine();
         int age = Integer.parseInt(s);
 
-        if (age >= 13) {
-            System.out.println("You can ride!");
+        if (age >= 18) {
+            System.out.println("Input your fav food:");
+            String food = sc.nextLine();
+
+            if (food.equals("pizza")) {
+                System.out.println("Mine too!");
+            } else {
+                System.out.println("Not mine");
+            }
+            System.out.println("You are an adult");
+        } else if (age >= 13){
+            System.out.println("You are not a teenager!");
         } else {
-            System.out.println("You can not ride!");
+            System.out.println("You are not a teenager or an adult");
         }
     }
 }
