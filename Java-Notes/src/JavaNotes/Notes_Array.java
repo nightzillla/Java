e243d259ee62d74f78aa4e11d258dfa27189711f
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-09 13:04:03 -0500

    finshed array

--- Java-Notes/src/JavaNotes/Notes_Array.java
+++ Java-Notes/src/JavaNotes/Notes_Array.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 
 public class Notes_Array {
     public static void main(String[] args) {
+//        int[] newArr = new int[5];
+        String[] newArr = new String[5];
+        newArr [0] = "hello";
+        newArr [1] = "hi";
+        newArr [2] = "tim";
+        newArr [3] = "bill";
+        newArr [4] = "joe";
+
+        int[] nums = {2,3,54,6,6};
+        int y = nums[4];
+        String x = newArr[4];
+        System.out.println(x);
 
     }
 }
