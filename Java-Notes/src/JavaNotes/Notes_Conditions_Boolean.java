cf86e4b950220bca73032fb8eba17e43df8ebce1
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-10-08 19:23:33 -0500

    finshed else if

--- Java-Notes/src/JavaNotes/Notes_Conditions_Boolean.java
+++ Java-Notes/src/JavaNotes/Notes_Conditions_Boolean.java
@@ -10,6 +10,8 @@ public class Notes_Conditions_Boolean {
         // > greater than,  < less than, == equal to,
         // >= greater than or equal to, <= less than or equal to
         // !== not equal to
+//        s.equals("hello"); // use this to compare two strings
+
 
         boolean compare = x == y;
 
--- Java-Notes/src/JavaNotes/Notes_If_Else.java
+++ Java-Notes/src/JavaNotes/Notes_If_Else.java
@@ -1,4 +1,19 @@
 package JavaNotes;
+import java.util.Scanner;
 
 public class Notes_If_Else {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        // s.equals("hello"); use this to compare two strings
+        // if you have a if, else if , else
+        // else has to be the last else if
+        if (s.equals("tim")) {
+            System.out.println("You type tim");
+        } else if (s.equals("Hello")) {
+            System.out.println("Hi");
+        } else {
+            System.out.println("Print");
+        }
+    }
 }
